cb7f036aa8e9a957cbc57d16fe4260fd8e1b6964
Leo Wong <gnowoel@gmail.com>
2012-01-23 02:55:45 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xcell::Application.config.secret_token = '256d9665bdf8514f581ca75ae8f5f27bc36da01eda31968a229ec554d7592d56ee6231417ba091de54750da6c62724b4e0bc7fafbfbec7a2fd883442bc95565f'
